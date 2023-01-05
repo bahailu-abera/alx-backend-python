bf603cd1c081221aea83989c1ee0a5e9a02d235f
bahailu <aberabahailu4@gmail.com>
2023-01-05 15:36:10 +0300

    sum mix

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module sum mixed list
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """
+    Returns the sum of mxd_list as a float
+    """
+    return sum(mxd_lst)
