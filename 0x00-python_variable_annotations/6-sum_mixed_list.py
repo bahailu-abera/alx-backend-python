cbf036b8f7884d4ecdfde1b42965b597ac832735
bahailu <aberabahailu4@gmail.com>
2023-01-05 15:44:19 +0300

    int float

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@ Module sum mixed list
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Returns the sum of mxd_list as a float
     """
