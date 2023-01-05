aee773eabeb25921b93236cc6609ba0899d3a7cc
bahailu <aberabahailu4@gmail.com>
2023-01-05 15:26:10 +0300

    list annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+Module sum list
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Returns the sum of input_list as float
+    """
+    return sum(input_list)
