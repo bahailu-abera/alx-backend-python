9cdd1e414fc492e464eb05183f19f14e4b32876a
bahailu <aberabahailu4@gmail.com>
2023-01-10 15:26:34 +0300

    generators

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ Module async generator
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Returns a generator of radom numbers
     """
