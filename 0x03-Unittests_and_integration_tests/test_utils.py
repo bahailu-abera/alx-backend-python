f31b1cf6c2f7dc23a4170abd82a61c075cbcf97e
Bahailu Abera <aberabahailu4@gmail.com>
2023-07-16 18:22:28 +0300

    test keyerror

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,8 +15,15 @@ class TestAccessNestedMap(unittest.TestCase):
         ("One path integer", {"a": 1}, ("a",), 1),
         ("one path dictionary", {"a": {"b": 2}}, ("a",), {"b": 2}),
         ("two path integer", {"a": {"b": 2}}, ("a", "b"), 2)
-        ])
+    ])
     def test_access_nested_map(self, name, nested_map, path, expected):
         """ Test that the method returns what it is supposed to. """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ("empty dict", {}, ("a",)),
+        ("key absence", {"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, name, nested_map, path):
+        """Test that exception raises for KeyError"""
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
