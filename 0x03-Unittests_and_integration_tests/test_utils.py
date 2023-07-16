5b24362c54fa5530c6acaad4445c62f6a1bac507
Bahailu Abera <aberabahailu4@gmail.com>
2023-07-16 18:48:56 +0300

    fix: remove the name for testcases

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,8 +8,6 @@ from typing import (
     Mapping,
     Sequence,
     Any,
-    Dict,
-    Callable,
 )
 from utils import access_nested_map
 
@@ -19,21 +17,22 @@ class TestAccessNestedMap(unittest.TestCase):
     Test for the access_nested_map method
     """
     @parameterized.expand([
-        ("One path integer", {"a": 1}, ("a",), 1),
-        ("one path dictionary", {"a": {"b": 2}}, ("a",), {"b": 2}),
-        ("two path integer", {"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, name: str, nested_map: Mapping,
+    def test_access_nested_map(self, nested_map: Mapping,
                                path: Sequence, expected: Any) -> None:
         """ Test that the method returns what it is supposed to. """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ("empty dict", {}, ("a",)),
-        ("key absence", {"a": 1}, ("a", "b"))
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
     ])
-    def test_access_nested_map_exception(self, name: str,
+    def test_access_nested_map_exception(self,
                                          nested_map: Mapping,
                                          path: Sequence) -> None:
         """Test that exception raises for KeyError"""
-        self.assertRaises(KeyError, access_nested_map, nested_map, path)
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
