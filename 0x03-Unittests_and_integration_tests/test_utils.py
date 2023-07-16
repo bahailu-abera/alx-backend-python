c5f1768dec39d5fd39553e0b9720dbc20f20ffcb
Bahailu Abera <aberabahailu4@gmail.com>
2023-07-16 18:32:58 +0300

    annotate methods

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,13 @@ A test for the access_nested_map method
 """
 import unittest
 from parameterized import parameterized
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
 from utils import access_nested_map
 
 
@@ -16,7 +23,8 @@ class TestAccessNestedMap(unittest.TestCase):
         ("one path dictionary", {"a": {"b": 2}}, ("a",), {"b": 2}),
         ("two path integer", {"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, name, nested_map, path, expected):
+    def test_access_nested_map(self, name: str, nested_map: Mapping,
+                               path: Sequence, expected: Any) -> None:
         """ Test that the method returns what it is supposed to. """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
@@ -24,6 +32,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ("empty dict", {}, ("a",)),
         ("key absence", {"a": 1}, ("a", "b"))
     ])
-    def test_access_nested_map_exception(self, name, nested_map, path):
+    def test_access_nested_map_exception(self, name: str,
+                                         nested_map: Mapping, path: Sequence) -> None:
         """Test that exception raises for KeyError"""
         self.assertRaises(KeyError, access_nested_map, nested_map, path)
